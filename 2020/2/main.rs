7b3b1ece0cb5304d16ebe96d1ba11660828aafbe
Christian Albrecht <christian.albrecht@mayflower.de>
2020-12-05 21:29:22 +0100

    Cleanup day 2 a bit

--- 2020/2/main.rs
+++ 2020/2/main.rs
@@ -8,32 +8,33 @@ fn main() -> Result<(), ()> {
 
     println!("input: {:#?}", input);
 
-    let data: Vec<(usize, usize, &str, &str, usize)> = input.split_terminator("\n")
+    let data: Vec<(usize, usize, usize, char, char, char)> = input.split_terminator("\n")
         .map(|x: &str| x.splitn(5, |c| c == '-' || c == ' ' || c == ':').collect())
-        .map(|x: Vec<&str>| (x[0].parse::<usize>().unwrap(),
-                             x[1].parse::<usize>().unwrap(),
-                             x[2],
-                             x[4],
-                             x[4].matches(x[2]).collect::<Vec<&str>>().len()))
+        .map(|x: Vec<&str>|
+             (x[0].parse::<usize>().unwrap(),
+              x[1].parse::<usize>().unwrap(),
+              x[4].matches(x[2]).collect::<Vec<&str>>().len(),
+              x[2].chars().nth(0).unwrap(),
+              x[4]))
+        .map(|x: (usize, usize, usize, char, &str)|
+             (x.0, x.1, x.2, x.3,
+              x.4.chars().nth(x.0 -1).unwrap_or('?'),
+              x.4.chars().nth(x.1 -1).unwrap_or('?')))
         .collect();
 
     println!("data: {:#?}", data);
 
     let lvl1: usize = data.iter()
-        .map(|x: &(usize, usize, &str, &str, usize)|
-             x.0 <= x.4 && x.1 >= x.4)
+        .map(|x: &(usize, usize, usize, char, char, char)|
+             x.0 <= x.2 && x.1 >= x.2)
         .filter(|x: &bool| *x)
         .collect::<Vec<bool>>()
         .len();
 
     let lvl2: usize = data.iter()
-        .map(|x: &(usize, usize, &str, &str, usize)|
-             (x.2.chars().nth(0).unwrap(),
-              x.3.chars().nth(x.0 -1).unwrap_or('?'),
-              x.3.chars().nth(x.1 -1).unwrap_or('?')))
-        .map(|x: (char, char, char)|
-             x.0 == x.1 && x.0 != x.2 ||
-             x.0 != x.1 && x.0 == x.2)
+        .map(|x: &(usize, usize, usize, char, char, char)|
+             x.3 == x.4 && x.3 != x.5 ||
+             x.3 != x.4 && x.3 == x.5)
         .filter(|x: &bool| *x)
         .collect::<Vec<bool>>()
         .len();
