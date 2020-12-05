d023f5922baffc9f874b5eac9c70085122ab4a48
Christian Albrecht <christian.albrecht@mayflower.de>
2020-12-05 17:14:04 +0100

    Comlete both levels day 1

--- 2020/1/main.rs
+++ 2020/1/main.rs
@@ -7,5 +7,37 @@ fn main() -> Result<(), ()> {
 
     println!("input: {:#?}", input);
 
+    let numbers: Vec<i32> = input.split("\n")
+        .filter_map(|x: &str| x.parse::<i32>().ok())
+        .collect::<Vec<i32>>();
+
+    let mut lvl1: i32 = 0;
+    let mut lvl2: i32 = 0;
+
+    'out: for n1 in &numbers {
+        for n2 in &numbers {
+            if n1 + n2 == 2020 {
+                lvl1 = n1 * n2;
+                if lvl2 != 0 {
+                    break 'out;
+                }
+            }
+            for n3 in &numbers {
+                if n1 + n2 + n3 == 2020 {
+                    lvl2 = n1 * n2 * n3;
+                    if lvl1 != 0 {
+                        break 'out;
+                    }
+                }
+            }
+        }
+    }
+
+    println!("level 1: {:#?}", lvl1);
+    println!("level 2: {:#?}", lvl2);
+
+    //println!("{:#?}", aoc_io::post_result_text(&client, "1", "1", lvl1.to_string().as_str()));
+    //println!("{:#?}", aoc_io::post_result_text(&client, "1", "2", lvl2.to_string().as_str()));
+
     Ok(())
 }
