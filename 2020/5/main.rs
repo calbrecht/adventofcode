d51a62df2fad8d98a390c13ea0df577ffca8f2f3
Christian Albrecht <christian.albrecht@mayflower.de>
2020-12-06 21:51:49 +0100

    Complete day 5 lvl 1

---
+++ 2020/5/main.rs
@@ -0,0 +1,44 @@
+extern crate aoc_io;
+
+fn seat(b: (i8, i8), s: &str) -> (i8, i8) {
+    //println!("u: {:#?} {:#?}", b, s);
+
+    match b.0 == b.1 && s.len() > 0 {
+        true => (b.0, seat((0, 7), &s[..]).1),
+        false => match &s.chars().nth(0) {
+            Some('B')|Some('R') => seat((b.1 - (b.1 - b.0)/2, b.1), &s[1..]),
+            Some('F')|Some('L') => seat((b.0, b.0 + (b.1 - b.0)/2), &s[1..]),
+            _ => b,
+        }
+    }
+}
+
+fn main() -> Result<(), ()> {
+    let day: &str = "5";
+    aoc_io::init_env();
+    let client = aoc_io::build_client().unwrap();
+    let input: String = aoc_io::fetch_input_text(&client, day).unwrap();
+    println!("input: {:#?}", input);
+
+    let data: Vec<&str> = input.split_terminator("\n").collect();
+    println!("data: {:#?}", data);
+
+    let mut lvl1 = data.iter()
+        .map(|s: &&str| seat((0, 127), s))
+        .map(|x: (i8, i8)| x.0 as i16 * 8 + x.1 as i16)
+        .collect::<Vec<i16>>();
+
+    lvl1.sort();
+
+    println!("level 1: {:#?}", lvl1);
+    //println!("level 2: {:#?}", lvl2);
+
+    //println!("{:#?}", aoc_io::post_result_text(&client, day, "1", lvl1.to_string().as_str()));
+    //println!("{:#?}", aoc_io::post_result_text(&client, day, "2", lvl2.to_string().as_str()));
+
+    Ok(())
+}
+
+//BFFFBBFRRR: row 70, column 7, seat ID 567.
+//FFFBBBFRRR: row 14, column 7, seat ID 119.
+//BBFFBBFRLL: row 102, column 4, seat ID 820.
