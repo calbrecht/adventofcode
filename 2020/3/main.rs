8693b5e0f11703928daaca0a86d0f82c6cc9fe50
Christian Albrecht <christian.albrecht@mayflower.de>
2020-12-06 12:44:44 +0100

    Completed day 3 lvl 2

--- 2020/3/main.rs
+++ 2020/3/main.rs
@@ -1,5 +1,16 @@
 extern crate aoc_io;
 
+fn traverse (data: &Vec<&str>, step_x: usize, step_y: usize) -> usize {
+    let range_x: Vec<usize> = (0..data[0].len()).collect();
+
+    range_x.iter().cycle().step_by(step_x).enumerate()
+        .take_while(|x: &(usize, &usize)| x.0 * step_y < data.len())
+        .map(|x: (usize, &usize)| data[x.0 * step_y].chars().nth(*x.1))
+        .filter(|x: &Option<char>| *x == Some('#'))
+        .collect::<Vec<Option<char>>>()
+        .len()
+}
+
 fn main() -> Result<(), ()> {
     let day: &str = "3";
     aoc_io::init_env();
@@ -8,21 +19,19 @@ fn main() -> Result<(), ()> {
     //println!("input: {:#?}", input);
 
     let data: Vec<&str> = input.split_terminator("\n").collect();
-    //println!("data: {:#?}", data);
+    println!("data: {:#?}", data);
 
-    let range_x: Vec<usize> = (0..data[0].len()).collect();
+    let lvl1: usize = traverse(&data, 3, 1);
 
-    let lvl1: usize = range_x.iter().cycle().step_by(3).enumerate()
-        .take_while(|x: &(usize, &usize)| x.0 < data.len())
-        .map(|x: (usize, &usize)| data[x.0].chars().nth(*x.1))
-        .filter(|x: &Option<char>| *x == Some('#'))
-        .collect::<Vec<Option<char>>>()
-        .len();
+    let lvl2: usize =
+        traverse(&data, 1, 1)
+        * lvl1
+        * traverse(&data, 5, 1)
+        * traverse(&data, 7, 1)
+        * traverse(&data, 1, 2);
 
     println!("level 1: {:#?}", lvl1);
-
-    //let lvl2: usize =
-    //println!("level 2: {:#?}", lvl2);
+    println!("level 2: {:#?}", lvl2);
 
     //println!("{:#?}", aoc_io::post_result_text(&client, day, "1", lvl1.to_string().as_str()));
     //println!("{:#?}", aoc_io::post_result_text(&client, day, "2", lvl2.to_string().as_str()));
